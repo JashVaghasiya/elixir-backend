7eb572cec8478d3fcad731ac53f752d22d6de7d9
jashvaghasiy <jashvaghasiya2000@gmail.com>
2021-03-26 14:28:52 +0530

    26/03/2021

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,135 @@
 import User from '../models/user.js'
+import Product from '../models/product.js'
+import Order from '../models/order.js'
+import State from '../models/state.js'
+import City from '../models/city.js'
+import Category from '../models/category.js'
+import Sub from '../models/sub.js'
+import Agency from '../models/agency.js'
 
-export const getUsers = async (req, res, next) => {
+export const getUserCount = async (req, res) => {
+    let data = []
+    await User.find({ role: "seller" }).count().then(res => {
+        data.push({
+            name: "Sellers",
+            count: res
+        })
+    })
+    await User.find({ role: "user" }).count().then(res => {
+        data.push({
+            name: "Users",
+            count: res
+        })
+    })
+    await User.find({ role: "doctors" }).count().then(res => {
+        data.push({
+            name: "Doctors",
+            count: res
+        })
+
+    })
+    res.json(data)
+}
+
+export const getOtherCount = async (req, res) => {
+
+    let data = []
+
+    await Sub.find({}).count().then(res => {
+        data.push({
+            name: "Sub Categories",
+            count: res
+        })
+    })
+    await Category.find({}).count().then(res => {
+        data.push({
+            name: "Categories",
+            count: res
+        })
+    })
+    await Agency.find({}).count().then(res => {
+        data.push({
+            name: "Agencies",
+            count: res
+        })
+
+    })
+    res.json(data)
+}
+
+
+export const getLocationCount = async (req, res) => {
+    let data = []
+    await State.find({}).count().then(res => {
+        data.push({
+            name: "States",
+            count: res
+        })
+    })
+    await City.find({}).count().then(res => {
+        data.push({
+            name: "Cities",
+            count: res
+        })
+    })
+    res.json(data)
+}
+
+export const getProductCount = async (req, res) => {
+    let data = []
+    await Product.find({}).count().then(res => {
+        data.push({
+            name: "Products",
+            count: res
+        })
+    })
+    await Product.find({ activated: true }).count().then(res => {
+        data.push({
+            name: "Active Products",
+            count: res
+        })
+    })
+    await Product.find({ activated: false }).count().then(res => {
+        data.push({
+            name: "Deactivate Products",
+            count: res
+        })
+    })
+    res.json(data)
+}
+
+export const getOrderCount = async (req, res) => {
+    let data = []
+    await Order.find({}).count().then(res => {
+        data.push({
+            name: "Orders",
+            count: res
+        })
+    })
+    await Order.find({ status: "delivered" }).count().then(res => {
+        data.push({
+            name: "Delivered Orders",
+            count: res
+        })
+    })
+    await Order.find({ status: "pending" }).count().then(res => {
+        data.push({
+            name: "Pending Orders",
+            count: res
+        })
+    })
+    res.json(data)
+}
+
+export const getUsers = async (req, res) => {
     try {
-        const user = await User.find({ role: 'user' })
-        res.json(user)
+        User.find({ role: 'user' }).exec((err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(result)
+            }
+        })
     } catch (err) {
         console.log(err)
         res.json({
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { deleteProduct } from '../controllers/product.js'
-import { getUsers, getSellers, getOrders, deactivateUser, activateUser } from '../controllers/admin.js'
-import { authCheck, adminCheck, sellerCheck } from '../middleware/auth.js'
+import { getUsers, getSellers, getOrders, deactivateUser, activateUser, getUserCount, getLocationCount, getOtherCount, getOrderCount, getProductCount } from '../controllers/admin.js'
+import { authCheck, adminCheck } from '../middleware/auth.js'
 
 const router = express.Router()
 
@@ -10,6 +10,12 @@ router.put('/admin/user/deactivate/:id', authCheck, adminCheck, deactivateUser)
 router.put('/admin/user/activate/:id', authCheck, adminCheck, activateUser)
 router.get('/admin/seller', authCheck, adminCheck, getSellers)
 router.get('/admin/orders', authCheck, adminCheck, getOrders)
+
+router.get('/admin/user/count', authCheck, adminCheck, getUserCount)
+router.get('/admin/product/count', authCheck, adminCheck, getProductCount)
+router.get('/admin/order/count', authCheck, adminCheck, getOrderCount)
+router.get('/admin/location/count', authCheck, adminCheck, getLocationCount)
+router.get('/admin/other/count', authCheck, adminCheck, getOtherCount)
 
 export default router
 
